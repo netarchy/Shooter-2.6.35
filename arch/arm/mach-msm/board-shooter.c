a59999b650a446622f13ac3e61bfcb0dd57f801d
cayniarb <charlton.danielk@gmail.com>
2011-08-14 00:32:41 -0400

    [msm] acpuclock-8x60.c: add overclock freqs and voltages

--- arch/arm/mach-msm/board-shooter.c
+++ arch/arm/mach-msm/board-shooter.c
@@ -314,7 +314,7 @@ static struct regulator_consumer_supply saw_s1_supply =
 static struct regulator_init_data saw_s0_init_data = {
 		.constraints = {
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 840000,
+			.min_uV = 800000,
 			.max_uV = MAX_VDD,
 		},
 		.num_consumer_supplies = 1,
@@ -324,7 +324,7 @@ static struct regulator_init_data saw_s0_init_data = {
 static struct regulator_init_data saw_s1_init_data = {
 		.constraints = {
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 840000,
+			.min_uV = 800000,
 			.max_uV = MAX_VDD,
 		},
 		.num_consumer_supplies = 1,
